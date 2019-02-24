cdcb6c8af8b8059bd7d2d58c57e7ae95d22d8398
小马哥 <mabingyao@gmail.com>
2019-02-24 23:46:35 +0800

    Create service_context.dart

---
+++ lib/src/rpc/service_context.dart
@@ -0,0 +1,16 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| service_context.dart                                     |
+|                                                          |
+| ServiceContext for Dart.                                 |
+|                                                          |
+| LastModified: Feb 23, 2019                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+part of hprose.rpc;
