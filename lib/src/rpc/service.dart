d4a3dc60b36c5bd280b545066bf84691ce0cf1c0
小马哥 <mabingyao@gmail.com>
2019-02-24 23:46:25 +0800

    Create service.dart

---
+++ lib/src/rpc/service.dart
@@ -0,0 +1,16 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| service.dart                                             |
+|                                                          |
+| hprose Service for Dart.                                 |
+|                                                          |
+| LastModified: Feb 23, 2019                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+part of hprose.rpc;
