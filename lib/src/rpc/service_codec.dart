18caf6b920cd07b08cb792e7162111c679445e4d
小马哥 <mabingyao@gmail.com>
2019-02-24 23:46:29 +0800

    Create service_codec.dart

---
+++ lib/src/rpc/service_codec.dart
@@ -0,0 +1,16 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| service_codec.dart                                       |
+|                                                          |
+| ServiceCodec for Dart.                                   |
+|                                                          |
+| LastModified: Feb 23, 2019                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+part of hprose.rpc;
