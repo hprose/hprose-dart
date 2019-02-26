bdd5ffe6138ca69d1e57f804fb976b2cc07692ce
小马哥 <mabingyao@gmail.com>
2019-02-26 19:00:53 +0800

    Update type_manager.dart

--- lib/src/io/type_manager.dart
+++ lib/src/io/type_manager.dart
@@ -8,18 +8,26 @@
 |                                                          |
 | hprose TypeManager for Dart.                             |
 |                                                          |
-| LastModified: Feb 16, 2019                               |
+| LastModified: Feb 26, 2019                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
 \*________________________________________________________*/
 
 part of hprose.io;
 
+typedef T FromJson<T>(Map<String, dynamic> data);
+
 class _TypeManager {
+  final Map<String, FromJson> _fromJson = {};
   final Map<String, Map<String, Type>> _types = {};
   final Map<String, Type> _emptyType = {};
-  void register(String name, Map<String, Type> fields) {
+  void register<T>(FromJson<T> fromJson, [Map<String, Type> fields]) {
+    var name = T.toString();
+    _fromJson[name] = fromJson;
     _types[name] = fields;
+    if (fromJson != null) {
+      Deserializer.register<T>(new ObjectDeserializer(name));
+    }
   }
 
   bool isRegister(String name) {
@@ -28,6 +36,10 @@ class _TypeManager {
 
   Map<String, Type> getType(String name) {
     return isRegister(name) ? _types[name] : _emptyType;
+  }
+
+  FromJson getConstructor(String name) {
+    return _fromJson[name];
   }
 }
 
