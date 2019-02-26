028b6b06e2fb1626d86080a0cc850457de8945b9
小马哥 <mabingyao@gmail.com>
2019-02-27 00:59:48 +0800

    Update service_context.dart

--- lib/src/rpc/core/service_context.dart
+++ lib/src/rpc/core/service_context.dart
@@ -15,9 +15,10 @@
 
 part of hprose.rpc.core;
 
-class ServiceContext extends Context {
+abstract class ServiceContext extends Context {
   final Service service;
   Method method;
-  
+  final Map<String, dynamic> requestHeaders = {};
+  final Map<String, dynamic> responseHeaders = {};
   ServiceContext(this.service);
 }
