edab479e4ead916d06b2b3d46ec52e6429123732
小马哥 <mabingyao@gmail.com>
2019-02-27 00:59:31 +0800

    Create service_context.dart

---
+++ lib/src/rpc/service_context.dart
@@ -0,0 +1,22 @@
+/*--------------------------------------------------------*\
+|                                                          |
+|                          hprose                          |
+|                                                          |
+| Official WebSite: https://hprose.com                     |
+|                                                          |
+| service_context.dart                                     |
+|                                                          |
+| ServiceContext for Dart.                                 |
+|                                                          |
+| LastModified: Feb 27, 2019                               |
+| Author: Ma Bingyao <andot@hprose.com>                    |
+|                                                          |
+\*________________________________________________________*/
+
+part of hprose.rpc;
+
+class ServiceContext extends core.ServiceContext {
+  InternetAddress address;
+  int port;
+  ServiceContext(Service service) : super(service);
+}
