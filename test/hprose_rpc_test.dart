5c9d35b05c3fffb951741b13089601d972049741
小马哥 <mabingyao@gmail.com>
2019-02-28 22:56:16 +0800

    Update hprose_rpc_test.dart

--- test/hprose_rpc_test.dart
+++ test/hprose_rpc_test.dart
@@ -3,8 +3,23 @@ library hprose_rpc_tests;
 import 'package:test/test.dart';
 import 'package:hprose/rpc_core.dart';
 
+String hello(String name) {
+  return 'hello $name';
+}
+
 void main() {
-  test('method', () {
+  test('hello world', () async {
+    final service = new Service();
+    service.addMethod(hello);
+    final server = new MockServer('test');
+    service.bind(server);
+    final client = new Client(['mock://test']);
+    final result = await client.invoke('hello', ['world']);
+    expect(result, equals('hello world'));
+    server.close();
+  });
+
+    test('method', () {
     print(new Method(
             <T>(Map<String, T> a1, int i, {List<Map<T, T>> a2, List a3}) => 10,
             'x')
@@ -16,4 +31,5 @@ void main() {
     print(new Method(<T>([T a1, T a2, double a3 = 12, int a4 = 32]) => a3 + a4, 'sum').positionalParameterTypes);
     print(new Method(<T>([T a1, T a2, double a3 = 12, int a4 = 32]) => a3 + a4, 'sum').optionalParameterTypes);
   });
+
 }
