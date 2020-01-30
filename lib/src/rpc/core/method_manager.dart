634f42436d70e9fbdbad72a7656f1b636a8fe883
andot <mabingyao@gmail.com>
2020-01-30 23:10:53 +0800

    Fixed MethodManager

--- lib/src/rpc/core/method_manager.dart
+++ lib/src/rpc/core/method_manager.dart
@@ -8,7 +8,7 @@
 |                                                          |
 | MethodManager for Dart.                                  |
 |                                                          |
-| LastModified: Dec 31, 2019                               |
+| LastModified: Jan 30, 2020                               |
 | Author: Ma Bingyao <andot@hprose.com>                    |
 |                                                          |
 \*________________________________________________________*/
@@ -20,39 +20,46 @@ typedef MissingMethod2 = dynamic Function(
     String fullname, List args, Context context);
 
 class MethodManager {
-  Map<String, Method> methods = {};
-  Iterable<String> getNames() => methods.keys;
+  List<String> _names = [];
+  Map<String, Method> _methods = {};
+  Iterable<String> getNames() => _names;
   Method get(String fullname) {
-    return methods.containsKey(fullname) ? methods[fullname] : methods['*'];
+    fullname = fullname.toLowerCase();
+    return _methods.containsKey(fullname) ? _methods[fullname] : _methods['*'];
   }
 
   void remove(String fullname) {
-    methods.remove(fullname);
+    _methods.remove(fullname.toLowerCase());
+    _names.remove(fullname);
   }
 
   void add(Method method) {
-    methods[method.fullname] = method;
+    final fullname = method.fullname;
+    _methods[fullname.toLowerCase()] = method;
+    if (_names.indexOf(fullname) == -1) {
+      _names.add(fullname);
+    }
   }
 
   void addMethod(Function method, [String fullname]) {
     add(Method(method, fullname: fullname));
   }
 
-  void addMethods(List<Function> methods, [List<String> fullnames]) {
+  void addMethods(List<Function> _methods, [List<String> fullnames]) {
     if (fullnames != null) {
-      for (var i = 0; i < methods.length; ++i) {
-        add(Method(methods[i], fullname: fullnames[i]));
+      for (var i = 0; i < _methods.length; ++i) {
+        add(Method(_methods[i], fullname: fullnames[i]));
       }
     } else {
-      for (var i = 0; i < methods.length; ++i) {
-        add(Method(methods[i]));
+      for (var i = 0; i < _methods.length; ++i) {
+        add(Method(_methods[i]));
       }
     }
   }
 
   void addMissingMethod<MissingMethod extends Function>(MissingMethod method) {
     if (method is MissingMethod1 || method is MissingMethod2) {
-      methods['*'] = Method(method, fullname: '*', missing: true);
+      add(Method(method, fullname: '*', missing: true));
     } else {
       throw ArgumentError('method is not a missing method');
     }
